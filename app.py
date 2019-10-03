b549ace07b20eafe77e9abb006aee43d3912b7ec
Khye Ean Low <lowkhyeean@gmail.com>
2019-10-04 00:42:11 +1000

    main logic done

---
+++ app.py
@@ -0,0 +1,13 @@
+import requests
+import json
+
+f = open("watchlist.json", "r");
+items = f.read();
+jsonList = json.loads(items);
+currentPrices = []
+for item in jsonList:
+    r = requests.get(url = item["url"])
+    dataInJson = r.json()
+    medianPrice = dataInJson["median_price"][2:]
+    currentPrices.append({item["name"]:medianPrice})
+print(currentPrices)
